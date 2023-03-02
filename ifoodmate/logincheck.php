95070bfe1cf583d8e115b36bbf3a555afe373648
romitshah02 <122880565+romitshah02@users.noreply.github.com>
2023-03-03 00:33:03 +0530

    Add files via upload

--- ifoodmate/logincheck.php
+++ ifoodmate/logincheck.php
@@ -3,34 +3,41 @@ require_once "connection.php";
 require_once "validate.php";
 
 
-$username = validate($_POST['USERNAME']);
-$password = validate($_POST['us_pwd']);
+//$username = json_decode($_POST['USERNAME']);
+//$password = json_decode($_POST['us_pwd']);
+$username = 'Alok15';
+$password = 'alok15';
 
-
-$sql = " SELECT * FROM `mst_user` WHERE `USERNAME` = '$username' AND `USER_PASSWORD` = '$password'  ";
-$sql2 = "SELECT * FROM `mst_provider` WHERE `SERVICE_PROVIDER_NAME` = '$username' AND `SERVICE_PROVIDER_PASSWORD` = '$password' ";
+$sql = " SELECT `USER_ID` FROM `mst_user` WHERE `USERNAME` = '$username' AND `USER_PASSWORD` = '$password'  ";
+$sql2 = "SELECT `SERVICE_PROVIDER_ID` FROM `mst_provider` WHERE `SERVICE_PROVIDER_NAME` = '$username' AND `SERVICE_PROVIDER_PASSWORD` = '$password' ";
 $result = $conn->query($sql);
-
+$return = array();
 
 if($result->num_rows > 0)
 {
-    echo "success";
+
+    $row = $result->fetch_assoc();
+    $temp['uid'] = $row['USER_ID'];
+    $temp['value'] = "success";    
 }
 else if ($result = $conn->query($sql2))
 {
     if ($result->num_rows > 0)
     {
-        echo "sp_success";
+        $row = $result->fetch_assoc();
+        $temp['uid'] = $row['SERVICE_PROVIDER_ID'];
+        $temp['value'] = "sp_success";
     }
     else
     {
-        echo "failure";
+        $temp['value'] = "failure";
     }
 }
 
+
+echo json_encode($temp);
 
 
 $conn->close();
 
 ?>
-
