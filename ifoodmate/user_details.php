f41af60534d7011c82dfbcb0be8aeefe15da626d
romitshah02 <122880565+romitshah02@users.noreply.github.com>
2023-03-04 00:04:53 +0530

    Add files via upload

--- ifoodmate/user_details.php
+++ ifoodmate/user_details.php
@@ -3,7 +3,7 @@
 require_once "connection.php";
 
 $incoming = file_get_contents("php://input");
-//$incoming = '{"uid":"001"}';
+//$incoming = '{"uid":"2"}';
 $data = json_decode($incoming,TRUE);
 $id = ($data['uid']);
 
@@ -14,18 +14,17 @@ if ($id == 001)
 }
 else
 {
-    $sql = "SELECT  `SERVICE_PROVIDER_NAME`,`ADDRESS`,`PHONE_NO`,`EMAIL`,`GST_NO` FROM `mst_provider` WHERE `SERVICE_PROVIDER_ID` = '$id' ";
+    $sql = "SELECT  `USERNAME`, `EMAIL`, `PHONE_NO`, `ADDRESS` FROM `mst_user` WHERE `USER_ID` = '$id' ";
     $result = $conn->query($sql);
     
     
     if ($result->num_rows > 0)
     {
         $row = $result->fetch_assoc();
-        $temp['name'] = $row['SERVICE_PROVIDER_NAME'];
+        $temp['uname'] = $row['USERNAME'];
         $temp['add'] = $row['ADDRESS'];
         $temp['pno'] = $row['PHONE_NO'];
         $temp['mail'] = $row['EMAIL'];
-        $temp['gst'] = $row['GST_NO'];
     }
     else
     {
